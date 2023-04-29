8e63b3e0713babf499f9d61b38f03455cc835530
neeschal00 <nischal06khatri@gmail.com>
2023-04-29 23:11:54 +0545

    setup for further usage

--- django_advertools/settings.py
+++ django_advertools/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'crispy_forms',
     'django_filters',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -139,4 +140,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
