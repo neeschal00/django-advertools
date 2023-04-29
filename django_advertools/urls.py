1824e4dbdb56aabec0ca3ce7b8c6c333b7e7b630
neeschal00 <nischal06khatri@gmail.com>
2023-04-29 20:36:59 +0545

    more setup

--- django_advertools/urls.py
+++ django_advertools/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+
+def index(request):
+    return render(request,"landing.html")
+
 
 urlpatterns = [
+    path('generate/', include('genra')),
     path('admin/', admin.site.urls),
 ]
