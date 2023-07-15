10a50a3838d137f4f218afeee2d417b06a2cd0bb
neeschal00 <nischal06khatri@gmail.com>
2023-07-15 19:45:27 +0545

    middleware passes the request

--- django_advertools/middleware.py
+++ django_advertools/middleware.py
@@ -1,22 +1,22 @@
 from django.template.response import TemplateResponse
 # middleware.py
 from django.conf import settings
-from django.http import HttpResponse
+import uuid
 
-class LoaderMiddleware(object):
-    def __init__(self, get_response):
-        print("LoaderMiddleware initialized")
-        self.get_response = get_response
+# class LoaderMiddleware(object):
+#     def __init__(self, get_response):
+#         print("LoaderMiddleware initialized")
+#         self.get_response = get_response
 
-    def process_request(self, request):
-        request.show_loader = True
-        return None
+#     def process_request(self, request):
+#         request.show_loader = True
+#         return None
 
-    def process_template_response(self, request, response):
-        if isinstance(response, TemplateResponse):
-            response.context_data["show_loader"] = request.show_loader
-            del request.show_loader
-        return response
+#     def process_template_response(self, request, response):
+#         if isinstance(response, TemplateResponse):
+#             response.context_data["show_loader"] = request.show_loader
+#             del request.show_loader
+#         return response
 
 
 
@@ -24,10 +24,10 @@ class CustomCookieMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def process_request(self, request):
-        if 'custom_cookie' not in request.COOKIES:
-            response = HttpResponse()
-            response.set_cookie('custom_cookie', 'cookie_value')
+    def __call__(self, request):
+        if 'socket_id' not in request.COOKIES:
+            response = self.get_response(request)
+            socket_id = uuid.uuid4()
+            response.set_cookie('socket_id',str(socket_id))
             return response
-
         return self.get_response(request)
